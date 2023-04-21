368dc87b4f891bb9e6c9e8d2bcd0d0bd65dc4b61
Richard Quizon <codenomics@proton.me>
2023-04-21 07:45:48 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Need this for the .env
+require('dotenv').config();
 // Require needs modules.
 const express = require('express');
 // Initialize the app object.
@@ -8,5 +10,15 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+// Create a 404 route. 
+// Starting point: After written out you can see it by doing local:3000/*.
+// Changing the HTML sting for the response body and setting the status code to 404.
+// Original: res.send('<h1>404 Page</h1>');
+// Update: res.status(404).send('<h1>404 Page</h1>');
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+});
+
 // Listen for connections.
-app.listen(3000)
+// Update: app.listen(3000) - After adding .env, changed to:
+app.listen(process.env.PORT);
